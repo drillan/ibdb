5f1d7bbdf1904f6d69e635c1b30c383a69db32f0
driller <eleshis@gmail.com>
2019-02-08 22:42:06 +0900

    String -> String(30)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ibdb",
-    version="0.0.0a1",
+    version="0.0.0a2",
     description="IB-insync to Database",
     author="driller",
     classifiers=[
@@ -11,6 +11,6 @@ setup(
         "Programming Language :: Python :: 3.7",
     ],
     packages=find_packages(),
-    install_requires=["ib-insync"],
+    install_requires=["ib-insync", "sqlalchemy"],
     python_requires='>=3.6, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
 )
